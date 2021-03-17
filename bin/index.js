315984024d80f2a69d897197f2512b033fe49998
Aadhi-N <36737474+Aadhi-N@users.noreply.github.com>
2021-03-17 17:22:27 -0400

    wrapped http function in promise; added switch cases to redirect user input

--- bin/index.js
+++ bin/index.js
@@ -1,40 +1,88 @@
 #!/usr/bin/env node
 
+const { resolveCname } = require('dns');
 const https = require('https');
+const { resolve, format } = require('path');
+const helpers = require('./helpers');
 const argv = process.argv.slice(2);
+const API = "https://worldtimeapi.org/api/timezone/";
 
 
-//Makes https request to API
-function apiRequest(argv, callback) {
-  https.get(`https://worldtimeapi.org/api/timezone/${argv}` , (res) => {
-    res.on('data', (fetchedApiData) => {
-      // process.stdout.write(fetchedApiData);
-      console.log("fetched data... printing to console")
-      callback(JSON.parse(fetchedApiData.toString()));
+/* Makes HTTPS request to API */
+function makeApiRequest(formattedStr) {
+  let url = formattedStr === null ? API : API.concat(formattedStr); //if requesting timezone, the defalt API url already contains it
+  return new Promise ((resolve, reject) => {
+    const req = https.get(url, (res) => {
+      let body = '';
+      res.on('data', (chunk) => {body += chunk.toString()}); 
+      res.on('error', reject); //does this error mean server returned with error?
+      res.on('end', () => {
+        if (res.statusCode >= 200 && res.statusCode <= 299) {
+          resolve({ statusCode: res.statusCode, headers: res.headers, body: body });
+        } else {
+          reject('Request failed. status: ' + res.statusCode);
+        }
+      })
+    })
+    req.on('error', reject);
+    req.setTimeout(3000, function() {
+      reject("Server connection timeout (after 3 seconds)")
+      this.abort();
     });
-  }).on('error', (e) => {
-    console.error(e);
+    req.end();
+  })
+};
+
+
+/* Redirects user input to either API request or helper function */
+const processUserInput = (argv) => {
+  return new Promise((resolve, reject) => {
+    const userInput = !argv.length ? null : argv.toString(); //convert input from array to string to match cases
+    
+    switch(userInput) {
+      case "-t":
+      case "--timezone":
+        resolve(null); //sending null because API is already formatted with 'timezone' in URL
+        break;
+
+      case "-h":
+      case "--help":
+        console.log(`List of commands ...`);
+        break;
+      
+      case null: 
+        reject("Provide an input")
+      default: 
+        let i = userInput.search(/\//g);
+        if (i !== -1) {
+          let subStr1 = userInput.substr(0, 1), subStr2 = userInput.substr(i+1, 1),
+          capitalized  = userInput.replace(subStr1, subStr1.toUpperCase()).replace(subStr2, subStr2.toUpperCase());
+          return resolve(capitalized);
+        } else {
+          console.log('nope')
+        }
+        break;
+    }
+  }) 
+};
+
+/* Display list of timezones in console */
+const displayTimeZones = (data) => {
+  //need to format data into tables... 
+}
+
+/* Executing promises one by one */
+const fetchDataPromise = (argv) => {
+  processUserInput(argv)
+  .then((formattedStr) => {
+    return makeApiRequest(formattedStr)
+  })
+  .then((apiResponse) => {
+    return displayTimeZones(apiResponse.body)
+  })
+  .catch((errorMsg) => {
+    console.log("Error:", errorMsg)
   })
 }
 
-// Prints to console
-function printLocalTime() {
-  const timeZoneLocation = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const cityName = timeZoneLocation.substring(timeZoneLocation.indexOf("/") + 1);
-  const timeZoneAbbr= new Date().toLocaleTimeString('en-us',{timeZoneName:'short'});
-  console.log("Fetching local time...")
-  console.log(`Your local time in ${cityName} is ${timeZoneAbbr}.\n\nUse the following commands to compare timezones:...`)
-}
-
-
-function fetchGlobalTime(argv, callback) {
-  console.log(`Fetching current time in ${argv[0]}...`)
-  apiRequest(argv[0], callback)
-}
-
-function printToConsole(result) {
-  console.log(`The time in ${result.timezone} is ${result.unixtime}.`)
-}
-
-// printLocalTime()
-fetchGlobalTime(argv, printToConsole)
+fetchDataPromise(argv);
